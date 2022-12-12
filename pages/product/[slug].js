d50d01df8539ce02d95ac28793142973b0d22a38
thdcodussongchaeyeon <thd0628@naver.com>
2022-12-12 23:07:41 +0900

    first commit

---
+++ pages/product/[slug].js
@@ -0,0 +1,104 @@
+import Image from "next/image";
+import Link from "next/link";
+import React, { useContext } from "react";
+import Layout from "../../components/Layout";
+//import data from "../../utils/data";
+import { Store } from "../../utils/Store";
+import { useRouter } from "next/router";
+import Product from "../../models/Product";
+import db from "../../utils/db";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+export default function ProductScreen({ product }) {
+  const { state, dispatch } = useContext(Store);
+  const router = useRouter();
+  //const { query } = useRouter();
+  //const { slug } = query;
+  //const product = data.products.find((x) => x.slug === slug);
+
+  if (!product) {
+    return <Layout title="Product Not Found">Product Not Found!</Layout>;
+  }
+
+  const addToCartHandler = async () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+
+    if (data.countInStock < quantity) {
+      return toast.error("Sorry. 재고가 없습니다");
+    }
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
+    router.push("/cart");
+  };
+
+  return (
+    <div>
+      <Layout title={product.name}>
+        <div className="py-2 font-bold">
+          <Link href="/">Back to product</Link>
+        </div>
+
+        <div className="grid md:grid-cols-4 md:gap-3">
+          <div className="md:col-span-2">
+            <Image
+              src={product.image}
+              alt={product.name}
+              width={640}
+              height={640}
+              layout="responsive"
+            />
+          </div>
+          <div>
+            <ul>
+              <li>
+                <h1 className="text-lg">{product.name}</h1>
+              </li>
+              <li>Category: {product.category}</li>
+              <li>Brand: {product.brand}</li>
+              <li>
+                {product.rating} of {product.numReviews}reviews
+              </li>
+              <li>Description: {product.description}</li>
+            </ul>
+          </div>
+          <div>
+            <div className="card p-5">
+              <div className="mb-2 flex justify-between">
+                <div>Price</div>
+                <div>${product.price}</div>
+              </div>
+              <div className="mb-2 flex justify-between">
+                <div>Status</div>
+                <div>
+                  {product.countInStock > 0 ? "In stock" : "Unavailable"}
+                </div>
+              </div>
+              <button
+                className="primary-button w-full"
+                onClick={addToCartHandler}
+              >
+                Add to cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+  return {
+    props: {
+      product: product ? db.convertDocToObj(product) : null,
+    },
+  };
+}
